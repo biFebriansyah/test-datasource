e1b792f08bf066e258eff2aba3c5799c8abb0ec6
ebiFebriansyah <bukanebi@gmail.com>
2022-07-12 20:14:03 +0700

    new: while check redis

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const { createClient } = require('redis')
 const amqp = require('amqplib/callback_api')
 const elasticseacrh = require('elasticsearch')
 
+// const redis = createClient({
+//    url: `redis://${process.env.REDIS_HOST}:6379`
+// })
 const redis = createClient({
-   url: `redis://${process.env.REDIS_HOST}:6379`
+   host: process.env.REDIS_HOST,
+   port: 6379
 })
 
 const elastic = new elasticseacrh.Client({
@@ -12,21 +16,30 @@ const elastic = new elasticseacrh.Client({
    apiVersion: 'master'
 })
 
-const checkRedis = async () => {
-   try {
-      await redis.connect()
-      const data = await redis.get('testkey')
-
-      if (data === 'OK' || data == null) {
-         console.log('redis connected')
-      } else {
-         console.log(data)
+const checkRedis = () => {
+   redis.setex('testkey', 30, 'hallo', (err, rep) => {
+      if (err) {
+         console.log(err)
+         return
       }
-      await redis.quit()
-   } catch (error) {
-      console.log(error)
-   }
+      console.log(rep)
+   })
 }
+// const checkRedis = async () => {
+//    try {
+//       await redis.connect()
+//       const data = await redis.get('testkey')
+
+//       if (data === 'OK' || data == null) {
+//          console.log('redis connected')
+//       } else {
+//          console.log(data)
+//       }
+//       await redis.quit()
+//    } catch (error) {
+//       console.log(error)
+//    }
+// }
 
 const checkAmqp = async () => {
    amqp.connect(`amqp://${process.env.AMQP_HOST}`, (err, conn) => {
@@ -41,7 +54,7 @@ const checkAmqp = async () => {
 const checkElastic = async () => {
    elastic.indices.create(
       {
-         index: 'test-elastic'
+         index: 'test-elastic2'
       },
       (err, res, status) => {
          if (err) {
@@ -53,6 +66,6 @@ const checkElastic = async () => {
    )
 }
 
-checkElastic()
+// checkElastic()
 checkRedis()
-checkAmqp()
+// checkAmqp()
